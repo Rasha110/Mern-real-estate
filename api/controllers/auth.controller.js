3e77e4f054fa7001a063e110cc179d962c5bcf10
rasha <fahimrsha3@gmail.com>
2025-07-14 22:56:49 +0500

    create sign up api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js"
+
+import bcryptjs from "bcryptjs";
+
+export const signup=async(req,res)=>{
+    const {username,email,password}=req.body;
+    //after body we are going to hash the password
+    const hashedPassword=await bcryptjs.hashSync(password,10);
+    //now to save this in database
+    const newUser= new User({username,email,password:hashedPassword});
+    //we set email unique but it sends error in terminal not to user to resoove this, use try-catch:
+    try{
+//now  save this user in database  as it takes time based on internet speedd so use await here
+    await newUser.save();
+    res.status(201).json("User created successfully!"); //201=>created!
+    }catch(err){
+        res.status(500).json(err.message);
+    }
+    
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 import userRouter from './routes/user.route.js'
 
@@ -15,7 +16,9 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
