151de3a2da52cb54ae90f0a511251fa67c58d9ea
rasha <fahimrsha3@gmail.com>
2025-07-15 19:55:33 +0500

    create sign in api rourte

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     //after body we are going to hash the password
@@ -16,4 +17,22 @@ export const signup=async(req,res,next)=>{
        next(err);
     }
     
+}
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+//now we'll check if the email is valid
+const validUser=await User.findOne({email});
+if(!validUser) return next(errorHandler (404,'User not found!'));
+const validPassword=bcryptjs.compareSync(password,validUser.password);
+if(!validPassword)  return next(errorHandler (401,'Wrong Credentials!')); 
+const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET) 
+//we dont want password shows in console/insomnia when we send request
+const {password:pass,...rest}=validUser._doc;
+//making cookie
+res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+    }
+    catch(err){
+
+    }
 }
