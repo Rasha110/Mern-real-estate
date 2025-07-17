47303440c11989e5047a46e2237134b225ffe538
rasha <fahimrsha3@gmail.com>
2025-07-17 23:13:27 +0500

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { updateUserStart,updateUserFailure,updateUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart,updateUserFailure,updateUserSuccess,deleteUserFailure,deleteUserStart,deleteUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 function Profile() {
   const dispatch=useDispatch();
@@ -90,6 +90,24 @@ const handleSubmit=async(e)=>{
 dispatch(updateUserFailure(error.message))
   }
 }
+
+const handleDelete=async()=>{
+  try{
+    dispatch(deleteUserStart());
+const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+  method: 'DELETE',
+
+})
+const data=await res.json();
+if(data.success===false){
+  return dispatch(deleteUserFailure(data.message));
+  return;
+}
+dispatch(deleteUserSuccess(data)) 
+  }catch(error){
+    dispatch(deleteUserFailure(error.message));
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -151,10 +169,10 @@ dispatch(updateUserFailure(error.message))
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDelete} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
-      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+    
        <p className='text-green-700 mt-5'>{updateSuccess ? 'user is updated successfully!' : ''}</p>
     </div>
   );
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -34,7 +34,19 @@ state.error=null;
   state.error = action.payload; 
 state.loading=false;
         },
+        deleteUserStart:(state)=>{
+          state.loading=true;
+        },
+        deleteUserSuccess:(state)=>{
+          state.currentUser=null;
+state.loading=false;
+state.error=null;
+        },
+        deleteUserFailure:(state,action)=>{
+  state.error = action.payload; 
+state.loading=false;
+        },
     }
  })
- export const {signInStart,signInFailure,signInSuccess,updateUserFailure,updateUserStart,updateUserSuccess}=userSlice.actions;
+ export const {signInStart,signInFailure,signInSuccess,updateUserFailure,updateUserStart,updateUserSuccess,deleteUserFailure,deleteUserStart,deleteUserSuccess}=userSlice.actions;
  export default userSlice.reducer;
