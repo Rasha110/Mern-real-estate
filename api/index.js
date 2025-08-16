f4e715eec5c6cea9cfc6a1938170d18f187702de
rasha <fahimrsha3@gmail.com>
2025-08-16 23:24:15 +0500

    index

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(
 app.use(cookieParser());
 
 // Routes
-app.get("/api/test", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Backend is running " });
 });
 app.use("/api/auth", authRouter);
